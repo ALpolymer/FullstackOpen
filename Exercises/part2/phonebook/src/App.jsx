6ae6803ce5e3520c10438da499480681b2252c1a
Alexandros Polymeros <alpolymer@gmail.com>
2025-01-26 12:44:07 +0200

    2.6 and 2.7 done

--- Exercises/part2/phonebook/src/App.jsx
+++ Exercises/part2/phonebook/src/App.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
+
 function App() {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [newName, setNewName] = useState("")
+  const addName = (e) => {
+    e.preventDefault()
+    if (persons.some((person) => person.name === newName.trim())) {
+      alert(`${newName} is already added to the phonebook`)
+      setNewName("")
+      return
+    } else {
+      const nameObj = { name: newName.trim() }
+      setPersons([...persons, nameObj])
+      setNewName("")
+    }
+  }
+
+  const handleNameChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
+  }
+  console.log("RENDERED!!!")
   return (
     <>
-      <h1> hello</h1>
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addName}>
+          <div>
+            name:{" "}
+            <input
+              placeholder="type your name"
+              value={newName}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        {persons.map((person) => {
+          return <div key={person.name}>{person.name}</div>
+        })}
+      </div>
     </>
   )
 }
