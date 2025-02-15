e63a9dad833cc80f478ba66eafec2fd3d4b14741
Alexandros Polymeros <alpolymer@gmail.com>
2025-02-15 16:36:17 +0200

    fetched weather icon

--- Exercises/part2/countries/src/Weather.jsx
+++ Exercises/part2/countries/src/Weather.jsx
@@ -1,29 +1,36 @@
 import { useEffect, useState } from "react"
 import { getCityWeatherData } from "./api"
-// import axios from "axios"
+import axios from "axios"
 // const key = import.meta.env.VITE_OPENWEATHERMAP_API_KEY
 // const baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
 
 const Weather = ({ country }) => {
-  const [weatherData, setWeatherData] = useState([])
+  const [weatherData, setWeatherData] = useState({ allData: null, icon: "" })
 
   const city = country.capital[0]
 
   useEffect(() => {
     getCityWeatherData(city).then((data) => {
-      setWeatherData(data)
+      setWeatherData({ allData: data, icon: data.weather[0].icon })
     })
-  }, [])
-  console.log(weatherData.wind.speed)
-  if (weatherData.length === 0) {
+  }, [city])
+
+  if (!weatherData.allData) {
     return <h1>Loading....</h1>
+  } else {
+    console.log("datafetched", weatherData)
   }
   return (
     <div>
       <h1>Weather in {city}</h1>
-      <p>Temperature: {weatherData.main.temp} Celsius</p>
+      <p>Temperature:{weatherData.allData.main.temp} Celsius</p>
 
-      <p>Wind: {weatherData.wind.speed} m/s</p>
+      <img
+        src={`http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+        alt="Weather icon"
+      />
+
+      <p>Wind: {weatherData.allData.wind.speed}m/s</p>
     </div>
   )
 }
