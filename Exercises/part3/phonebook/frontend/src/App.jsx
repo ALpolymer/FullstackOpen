4a48eb367b661d5faec5a1f6863662ad87ce3bbc
Alexandros Polymeros <alpolymer@gmail.com>
2025-03-03 18:25:21 +0200

    scaffolded frontend structure

---
+++ Exercises/part3/phonebook/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react"
+
+import Filter from "./components/Filter"
+import Messages from "./components/Messages"
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
